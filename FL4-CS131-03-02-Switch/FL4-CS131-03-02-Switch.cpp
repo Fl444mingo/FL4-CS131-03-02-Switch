16c3e1ed4e473d59616ad380ed7abf6689a00e7d
FL444mingo <thespectralpotato@gmail.com>
2025-03-26 17:27:13 -0700

    Add project files.

---
+++ FL4-CS131-03-02-Switch/FL4-CS131-03-02-Switch.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	char series;
+	cout << "Which Star Trek series? ([O]riginal,[T]NG,[D]S9,[V]oyager)? ";
+	cin >> series;
+	// You can use a switch to make decisions based on int or char values
+	switch (series) {
+	case 'O': case'o':
+		cout << "Captain: Kirk\n";
+		break;
+	case 'T': case 't':
+		cout << "Captain: Picard\n";
+		break;
+	case'D': case 'd':
+		cout << "Captain: Sisko\n";
+		break;
+	case 'V': case 'v':
+		cout << "Captain: Janeway\n";
+		break;
+	}
+	system("pause");
+	return 0;
+}
